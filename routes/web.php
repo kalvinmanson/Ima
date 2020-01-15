327508bf4a21ef422e7e7ee90293c9b50f0e02cd
Gustavo Barragan <dev@droni.co>
2020-01-14 22:24:02 -0500

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::prefix('admin')->namespace('Admin')->as('admin.')->middleware(['auth', 'admin'])->group(function () {
+    Route::get('/', 'CategoryController@index');
+    Route::resource('categories', 'CategoryController');
+    Route::resource('attachments', 'AttachmentController');
+    Route::resource('pages', 'PageController');
+    Route::resource('fields', 'FieldController');
+    Route::resource('notifications', 'NotificationController');
+    Route::resource('contacts', 'ContactController');
+    Route::resource('blocks', 'BlockController');
+    Route::resource('users', 'UserController');
+});
+
+Auth::routes();
+
+Route::get('/home', 'WebController@index')->name('home');
+Route::get('/', 'WebController@index')->name('home');
